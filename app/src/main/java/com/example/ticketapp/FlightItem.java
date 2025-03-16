65d6627957178d9ebd70d457aa2295b4cce01b5b
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-03-17 00:12:44 +0800

    Updated FlightItem.java, SearchFlightActivity.java Added fields "origin" and "destination", along with their getter methods. Added the corresponding missing fields in FlightListAdapter due to this change.

--- app/src/main/java/com/example/ticketapp/FlightItem.java
+++ app/src/main/java/com/example/ticketapp/FlightItem.java
@@ -3,15 +3,17 @@ package com.example.ticketapp;
 public class FlightItem {
     private final String airline_name;
     private final int arrival_time, departure_time;
-    private final String flight_number;
+    private final String destination, flight_number, origin;
     private final double price;
     private final Class<?> targetActivity;
 
-    public FlightItem(String airline_name, int arrival_time, int departure_time, String flight_number, double price, Class<?> targetActivity) {
+    public FlightItem(String airline_name, int arrival_time, int departure_time, String destination, String flight_number, String origin, double price, Class<?> targetActivity) {
         this.airline_name = airline_name;
         this.arrival_time = arrival_time;
         this.departure_time = departure_time;
+        this.destination = destination;
         this.flight_number = flight_number;
+        this.origin = origin;
         this.price = price;
         this.targetActivity = targetActivity;
     }
@@ -28,8 +30,16 @@ public class FlightItem {
         return departure_time;
     }
 
+    public String getDestination() {
+        return destination;
+    }
+
     public String getFlight_number() {
         return flight_number;
+    }
+
+    public String getOrigin() {
+        return origin;
     }
 
     public double getPrice() {

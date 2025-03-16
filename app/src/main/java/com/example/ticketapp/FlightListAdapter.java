e8bd3c4eea763ad16c05bd6f5b3d731be359a53e
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-03-17 00:45:57 +0800

    Updated FlightListAdapter.java Made the getView method able to set a list of data in the view to add dynamism to the application.

--- app/src/main/java/com/example/ticketapp/FlightListAdapter.java
+++ app/src/main/java/com/example/ticketapp/FlightListAdapter.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class FlightListAdapter extends BaseAdapter {
     private final Context context;
@@ -35,12 +38,54 @@ public class FlightListAdapter extends BaseAdapter {
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.flight_list_layout, parent, false);
         }
+
+        TextView airline = convertView.findViewById(R.id.label_airline),
+                destination = convertView.findViewById(R.id.label_destination),
+                duration = convertView.findViewById(R.id.label_duration),
+                end_time = convertView.findViewById(R.id.label_end_time);
         FlightItem item = items[position];
+        TextView origin = convertView.findViewById(R.id.label_origin),
+                price = convertView.findViewById(R.id.txt_price),
+                start_time = convertView.findViewById(R.id.label_start_time);
+
+        int int_end_time = item.getArrival_time();
+        int int_start_time = item.getDeparture_time();
+
+        airline.setText(item.getAirline_name());
+        destination.setText(item.getDestination());
+        duration.setText(calculateDuration(int_start_time, int_end_time));
+        end_time.setText(String.format(new Locale("en"), "%d:%d", int_end_time / 100, int_end_time % 100));
+        origin.setText(item.getOrigin());
+        price.setText(String.format(new Locale("en"), "$%.2f", item.getPrice()));
+        start_time.setText(String.format(new Locale("en"), "%d:%d", int_start_time / 100, int_start_time % 100));
+
         convertView.setOnClickListener(v -> {
             Intent intent = new Intent(context, item.getTargetActivity());
             context.startActivity(intent);
         });
 
         return convertView;
+    }
+
+    private String calculateDuration(int start_time, int end_time) {
+        String txt_duration;
+        int start_hrs = start_time / 100, start_mins = start_time % 100;
+        int end_hrs = end_time / 100, end_mins = end_time % 100;
+        int duration_hrs, duration_mins;
+
+        if (end_mins >= start_mins) {
+            duration_mins = end_mins - start_mins;
+        } else {
+            duration_mins = end_mins + 60 - start_mins;
+            end_hrs -= 1;
+        }
+
+        if (end_hrs >= start_hrs) {
+            duration_hrs = end_hrs - start_hrs;
+        } else {
+            duration_hrs = end_hrs + 24 - start_hrs;
+        }
+        txt_duration = String.format(new Locale("en"), "%d HRS %d MINS", duration_hrs, duration_mins);
+        return txt_duration;
     }
 }
